77f35bdbd09725f93c72ecd9a13c5ccf100572a7
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-11-20 15:57:12 -0800

    Separated flash[:notice] and flash[:suggestion] to protect against injection attacks

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,15 +5,22 @@ class StaticPagesController < ApplicationController
         @flickr = Flickr.new
         @params = {user_id: params[:user_id], page: params[:page]}
         @photos = @flickr.photos.search(@params)
-        flash.now[:notice] = "No photos found for user ID# #{params[:user_id]}. Why not try #{random_user}?" if @photos.none?
+        if @photos.none?
+          flash.now[:notice] = "No photos found for User ID #{params[:user_id]}."
+          flash.now[:suggestion] = "Why not try #{random_user}?"
+        end
       rescue Flickr::FailedResponse => e
-        flash.now[:notice] = "No user found with ID# #{params[:user_id]}. Why not try #{random_user}?"
+        flash.now[:notice] = "No user found with ID #{params[:user_id]}."
+        flash.now[:suggestion] = "Why not try #{random_user}?"
       end
     elsif params[:tags]
       @flickr = Flickr.new
       @params = {tags: params[:tags], tag_mode: 'all', page: params[:page], sort: 'relevance'}
       @photos = @flickr.photos.search(@params)
-      flash.now[:notice] = "No photos found with the given tags. (Separate tags with commas.) Why not try #{random_tag}?" unless @photos.any?
+      if @photos.none?
+        flash.now[:notice] = "No photos found with the given tags. (Separate tags with commas.)"
+        flash.now[:suggestion] = "Why not try #{random_tag}?"
+      end
     end
   end
 
@@ -24,7 +31,7 @@ class StaticPagesController < ApplicationController
   private
 
   def random_user
-    suggestion = @flickr.interestingness.getList(per_page: 1, page: (rand(30) + 1)).first.owner
+    suggestion = @flickr.interestingness.getList(per_page: 1, page: (rand(100) + 1)).first.owner
     view_context.link_to suggestion, search_path(params: { user_id: suggestion })
   end
 
