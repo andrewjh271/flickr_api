b0c7810ac403fe7d9649ffe45ef7fede7ef36fc2
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-11-19 18:25:29 -0800

    Added search by tags functionality and updated styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,23 @@
 class StaticPagesController < ApplicationController
   def search
+    # fail
     if params[:user_id]
       begin
-        @photos = Flickr.new.photos.search(user_id: params[:user_id])
+        @params = {user_id: params[:user_id], page: params[:page]}
+        @photos = Flickr.new.photos.search(@params)
         flash.now[:notice] = 'No photos found with the given user id.' unless @photos.any?
       rescue Flickr::FailedResponse => e
         flash.now[:notice] = "No user found with ID# #{params[:user_id]}."
       end
+    elsif params[:tags]
+      @params = {tags: params[:tags], tag_mode: 'all', page: params[:page]}
+      @photos = Flickr.new.photos.search(@params)
+      flash.now[:notice] = 'No photos found with the given tags.' unless @photos.any?
     end
+  end
+
+  def about
+    
   end
 end
 
